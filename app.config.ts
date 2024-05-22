5febffe2be385bf810ef32211a63d390c012f72a
Rahuletto <rahulmarban@gmail.com>
2024-05-22 20:07:07 +0530

    publish?

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "@solidjs/start/config";
-import solid from "solid-start/vite"
-import vercel from "solid-start-vercel"
 
 export default defineConfig({
-    vite: {
-        plugins: [solid({ adapter: vercel() })]
-    }
+    server: {
+        preset: "vercel",
+      },
 });
