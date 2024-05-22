43c3051a9f759ae9988203228eeb7873b9ee2cd2
Rahuletto <rahulmarban@gmail.com>
2024-05-22 20:04:14 +0530

    vercel sucks

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,9 @@
 import { defineConfig } from "@solidjs/start/config";
+import solid from "solid-start/vite"
+import vercel from "solid-start-vercel"
 
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        plugins: [solid({ adapter: vercel() })]
+    }
+});
